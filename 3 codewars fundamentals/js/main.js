5d7b60ab7624c5c95d519fc0115c8820c19cb0ea
Cassie Capewell <cassie.capewell@gmail.com>
2021-02-19 17:01:56 -0500

    3 codewars fundamentals

---
+++ 3 codewars fundamentals/js/main.js
@@ -0,0 +1,24 @@
+// string repeat
+function repeatStr (n, s) {
+let repeatedString = "";
+for(let i=0; i<n; i++)
+  repeatedString += s;
+  return repeatedString;
+}
+
+// Convert boolean values to strings 'Yes' or 'No'.
+  if (bool == true) {
+    return "Yes"
+  }else if (bool == false){
+    return "No"
+  }
+}
+
+// return negative
+function makeNegative(num) {
+  if(num > 0){
+    return (-num)
+  }else{
+    return num
+  }
+}
