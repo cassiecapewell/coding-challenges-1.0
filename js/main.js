47b39fe8b07657840fe3a380cca2f03b34450176
Cassie Capewell <cassie.capewell@gmail.com>
2021-04-24 20:22:39 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function multiply(n1,n2){
   return n1*n2
 }
-// refactored
+// refactored the above
 let multiply = (a, b) => a * b;
 
 // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
